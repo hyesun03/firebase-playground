0c4564235f446828e2e14f30ba37790bdd355040
hyesun03 <chsun0303@gmail.com>
2017-08-03 15:27:58 +0900

    해시값으로 생성됨?

--- public/index.js
+++ public/index.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  writeData("uid_01", "hyesun01", "chsun0101@naver.com");
+  writeData("hyesun01", "chsun0101@naver.com");
+  writeData("hyesun02", "chsun0202@naver.com");
+  writeData("hyesun03", "chsun0303@naver.com");
+  writeData("hyesun04", "chsun0404@naver.com");
+  writeData("hyesun05", "chsun0505@naver.com");
 
 });
 
-function writeData(userId, name, email) {
-  firebase.database().ref('users/' + userId).push({
+function writeData(name, email) {
+  firebase.database().ref('users/').push({
     username: name,
     email: email
   });
